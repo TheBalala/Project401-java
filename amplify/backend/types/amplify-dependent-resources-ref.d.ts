b4ed0fcec44298b82a0d182fb7669ba35981b100
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-12-11 18:52:30 +0200

    add relation

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,8 +9,6 @@ export type AmplifyDependentResourcesAttributes = {
         "finalproject7dade171": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
