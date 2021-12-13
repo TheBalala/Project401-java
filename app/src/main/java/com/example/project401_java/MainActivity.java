4893f05778e23fb8d79743ce7e44ba6f9a41acf5
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-12-13 23:29:46 +0200

    did all the functionality for the normal user and finish 85% for the admin

--- app/src/main/java/com/example/project401_java/MainActivity.java
+++ app/src/main/java/com/example/project401_java/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project401_java;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.api.graphql.model.ModelMutation;
@@ -44,6 +46,7 @@ User user;
         } catch (AmplifyException error) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
         }
+
         Amplify.Auth.fetchAuthSession(
                 result -> Log.i("AmplifyQuickstart", result.toString()),
                 error -> Log.e("AmplifyQuickstart", error.toString())
@@ -57,87 +60,6 @@ User user;
             }
         });
 
-
-
-
-
-
-
-
-//
-//
-//        Category c1 = Category.builder()
-//                .categoryName("electric")
-//                .cityName("amman")
-//                .build();
-////
-//        Amplify.API.mutate(ModelMutation.create(c1),
-//                response -> Log.i("MyAmplifyApp", "Todo with id: " + response.getData().getId()),
-//                error -> Log.e("MyAmplifyApp", "Create failed", error)
-//        );
-
-//        Complain c1 = Complain.builder()
-//                .description("jhb skejfnew kjnsf sfdjnsdn")
-//                .state("sent")
-//                .cityName("amman")
-//                .categoryName("electric")
-//                .username("alaa")
-//                .build();
-//////
-//        Amplify.API.mutate(ModelMutation.create(c1),
-//                response -> Log.i("MyAmplifyApp", "Todo with id: " + response.getData().getId()),
-//                error -> Log.e("MyAmplifyApp", "Create failed", error)
-//        );
-//        Amplify.API.query(
-//                ModelQuery.list(Complain.class, Complain.USERNAME.contains("alaa")),
-//                response -> {
-//                    for (Complain complain : response.getData()) {
-//                        coco = complain;
-//                        Log.i("MyAmplifyApp", complain.getUsername());
-//                    }
-//                    System.out.println("dddddddddddddddddddddddddddd"+coco.toString());
-//                },
-//                error -> Log.e("MyAmplifyApp", "Query failure", error)
-//        );
-
-
-//
-//        Category c2 = Category.builder()
-//                .name("street")
-//                .build();
-//
-//        Amplify.API.mutate(ModelMutation.create(c2),
-//                response -> Log.i("MyAmplifyApp", "Todo with id: " + response.getData().getId()),
-//                error -> Log.e("MyAmplifyApp", "Create failed", error)
-//        );
-//        Category c3 = Category.builder()
-//                .name("water")
-//                .build();
-//
-//        Amplify.API.mutate(ModelMutation.create(c3),
-//                response -> Log.i("MyAmplifyApp", "Todo with id: " + response.getData().getId()),
-//                error -> Log.e("MyAmplifyApp", "Create failed", error)
-//        );
-//        Category c4 = Category.builder()
-//                .name("others")
-//                .build();
-//
-//        Amplify.API.mutate(ModelMutation.create(c4),
-//                response -> Log.i("MyAmplifyApp", "Todo with id: " + response.getData().getId()),
-//                error -> Log.e("MyAmplifyApp", "Create failed", error)
-//        );
-
-
-//        logOut.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Amplify.Auth.signOut(
-//                        AuthSignOutOptions.builder().globalSignOut(true).build(),
-//                        () -> Log.i("AuthQuickstart", "Signed out globally"),
-//                        error -> Log.e("AuthQuickstart", error.toString())
-//                );
-//            }
-//        });
         Button login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,14 +68,7 @@ User user;
                startActivity(intent);
             }
         });
-//        Button category = (Button) findViewById(R.id.category);
-//        category.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this,Category.class);
-//                startActivity(intent);
-//            }
-//        });
+
         Button city = (Button) findViewById(R.id.city);
         city.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -174,9 +89,12 @@ User user;
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.my:
-                Intent setting = new Intent(MainActivity.this, Profile.class);
-                startActivity(setting);
-                return true;
+                if (!AWSMobileClient.getInstance().getUsername().equals("admin")) {
+                    Intent setting = new Intent(MainActivity.this, Profile.class);
+                    startActivity(setting);
+                    return true;
+                }
+
             case R.id.logoutmenu:
                 Amplify.Auth.signOut(
                         AuthSignOutOptions.builder().globalSignOut(true).build(),
@@ -187,5 +105,15 @@ User user;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.my);
+        if (AWSMobileClient.getInstance().getUsername().equals("admin")) {
+            item.setTitle("");
+        }else {
+            item.setTitle("profile");
+        }
+        return super.onPrepareOptionsMenu(menu);
     }
 }
