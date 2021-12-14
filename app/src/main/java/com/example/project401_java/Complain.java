173e9047895cf38769326b16d61a26e4fdd0d3cf
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-12-14 13:50:33 +0200

    add activity

--- app/src/main/java/com/example/project401_java/Complain.java
+++ app/src/main/java/com/example/project401_java/Complain.java
@@ -39,44 +39,37 @@ com.amplifyframework.datastore.generated.model.Complain complain;
         String username = sharedPreferences.getString("username","username");
         saveComplain.setOnClickListener(v -> {
 
-             complain = com.amplifyframework.datastore.generated.model.Complain.builder()
-                    .description(description.getText().toString())
-                     .state("sent")
-                     .categoryName(category)
-                     .cityName(cityName)
-                     .username(username)
-                    .build();
 
-            Amplify.API.mutate(ModelMutation.create(complain),
-                    response -> Log.i("complain", "complain success " + response.getData().getId()),
-                    error -> Log.e("complain", "complain failed", error)
-            );
-//                description.getText();
         });
-//        Button button = findViewById(R.id.add);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(Complain.this, "submitted!", Toast.LENGTH_LONG).show();
-//                if (fileName != null) {
-//                    Amplify.Storage.uploadInputStream(
-//                            fileName,
-//                            inputStream,
-//                            results -> Log.i("MyAmplifyApp", "Successfully uploaded: " + results.getKey()),
-//                            storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure)
-//                    );
-//                }
-//                t = com.amplifyframework.datastore.generated.model.Complain.builder().(menuView.getText().toString()).title(mtask.getText().toString()).body(dtask.getText().toString()).state(stask.getText().toString()).file(fileName).build();
-//                /*---------------------------------------------------*/
-//
-//                Amplify.API.mutate(
-//                        ModelMutation.create(t),
-//                        response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
-//                        error -> Log.e("MyAmplifyApp", "Create failed", error)
-//                );
-//                finish();
-//            }
-//        });
+        Button button = findViewById(R.id.addphoto);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Complain.this, "submitted!", Toast.LENGTH_LONG).show();
+                if (fileName != null) {
+                    Amplify.Storage.uploadInputStream(
+                            fileName,
+                            inputStream,
+                            results -> Log.i("MyAmplifyApp", "Successfully uploaded: " + results.getKey()),
+                            storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure)
+                    );
+                }
+                complain = com.amplifyframework.datastore.generated.model.Complain.builder()
+                        .description(description.getText().toString())
+                        .state("sent")
+                        .categoryName(category)
+                        .cityName(cityName)
+                        .username(username)
+                        .fileUrl(fileName)
+                        .build();
+
+                Amplify.API.mutate(ModelMutation.create(complain),
+                        response -> Log.i("complain", "complain success " + response.getData().getId()),
+                        error -> Log.e("complain", "complain failed", error)
+                );
+                finish();
+            }
+        });
 
         findViewById(R.id.addphoto).setOnClickListener(new View.OnClickListener() {
             @Override
